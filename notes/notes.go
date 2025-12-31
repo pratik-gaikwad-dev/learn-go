6a7323ca9c07af2bcd8eb773aa2533e247808c07
Pratik Gaikwad <pratikgaikwad1004@gmail.com>
2025-12-31 18:00:57 +0530

    updated todos and notes with array json

--- notes/notes.go
+++ notes/notes.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"fmt"
 	"os"
-	"strings"
 	"time"
 
 	"github.com/google/uuid"
@@ -40,17 +39,82 @@ func (n *Note) Print() {
 	fmt.Println("Time: ", n.Time)
 }
 
-func (n *Note) Save() error {
-	fileName := strings.ReplaceAll(n.Title, " ", "_")
-	fileName = strings.ToLower(fileName) + ".json"
+func (n *Note) PrintAll() error {
+	fileName := "notes.json"
 
-	jsonData, err := json.MarshalIndent(n, "", "  ")
+	_, err := os.Stat(fileName)
 
 	if err != nil {
 		return err
 	}
 
+	readFile, readErr := os.ReadFile(fileName)
+
+	if readErr != nil {
+		return readErr
+	}
+
+	var notes []Note
+
+	unmErr := json.Unmarshal(readFile, &notes)
+
+	if unmErr != nil {
+		return err
+	}
+	for i := 1; i < len(notes)+1; i++ {
+		fmt.Println("Note: ", i)
+		fmt.Println("\t ID: ", notes[i-1].Id)
+		fmt.Println("\t Title: ", notes[i-1].Title)
+		fmt.Println("\t Content: ", notes[i-1].Content)
+		fmt.Println("\t Time: ", notes[i-1].Time)
+	}
+	return nil
+}
+
+func (n *Note) Save() error {
+	fileName := "notes.json"
+
+	_, err := os.Stat(fileName)
+
+	if err != nil {
+		if os.IsNotExist(err) {
+			crt, crtErr := os.Create(fileName)
+			if crtErr != nil {
+				return crtErr
+			}
+			crt.WriteString("[]")
+			crt.Close()
+		} else {
+			return err
+		}
+	}
+
+	readFile, readErr := os.ReadFile(fileName)
+
+	if readErr != nil {
+		return readErr
+	}
+
+	var notes []Note
+
+	unmErr := json.Unmarshal(readFile, &notes)
+
+	if unmErr != nil {
+		return err
+	}
+	notes = append(notes, *n)
+
+	jsonData, marshalErr := json.MarshalIndent(notes, "", "  ")
+
+	if marshalErr != nil {
+		return marshalErr
+	}
+
 	writeErr := os.WriteFile(fileName, jsonData, 0644)
 
-	return writeErr
+	if writeErr != nil {
+		return writeErr
+	}
+
+	return nil
 }
